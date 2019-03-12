6073e1a77368becafb2b91d971f67f37058bcc22
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-12 06:03:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,4 +3,13 @@ require 'pry'
 class Song
   @@count = 0 
   attr_accessor(:name, :artist, :genre)
+  
+  def initialize 
+    @@count += 1
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
 end 
